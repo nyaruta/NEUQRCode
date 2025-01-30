33e5fab68298c8231a2184d847f24f1cf99b42ed
Kirikaze Chiyuki <me@chyk.ink>
2025-01-30 17:08:39 +0800

    drop unused 2.x api models and methods

--- app/src/main/java/ink/chyk/neuqrcode/neu/NEUPass.kt
+++ app/src/main/java/ink/chyk/neuqrcode/neu/NEUPass.kt
@@ -1,6 +1,5 @@
 package ink.chyk.neuqrcode.neu
 
-import android.util.*
 import ink.chyk.neuqrcode.*
 import kotlinx.coroutines.*
 import okhttp3.*
@@ -15,45 +14,6 @@ class NEUPass {
     return request
       .header("X-Requested-With", "com.sunyt.testdemo")
       .header("X-App-Version", R.string.simulated_sunyt_version.toString())
-  }
-
-  @Deprecated("Deprecated in 3.x api")
-  suspend fun loginPortalTicket(studentId: String, password: String): String {
-    // 登录账号（智慧东大 2.x API，已经弃用）
-    Log.w("NEUPass", "loginPortalTicket deprecated, should be replaced with loginPersonalTicket")
-
-    val url = "https://portal.neu.edu.cn/mobile/api/auth/tickets"
-
-    val client = OkHttpClient.Builder()
-      .followRedirects(false)
-      .build()
-
-    val requestBody = FormBody.Builder()
-      .add("username", studentId)
-      .add("password", password)
-      .build()
-
-    val request = useRequestedWith(
-      Request.Builder()
-        .url(url)
-        .post(requestBody)
-    ).build()
-
-    return withContext(Dispatchers.IO) {
-      client.newCall(request).execute().use { response ->
-        if (response.isSuccessful) {
-          val body = response.body?.string()
-          if (body?.startsWith("TGT") == true) {
-            // 登录成功
-            body
-          } else {
-            throw PasswordIncorrectException()
-          }
-        } else {
-          throw PasswordIncorrectException()
-        }
-      }
-    }
   }
 
   suspend fun loginPersonalTicket(studentId: String, password: String): String {
@@ -133,15 +93,6 @@ class NEUPass {
     return loginNEUAppTicket(portalTicket, "https://ecode.neu.edu.cn/ecode/api/sso/login")
   }
 
-  @Deprecated("Deprecated in 3.x api")
-  suspend fun loginMobileApiTicket(portalTicket: String): String {
-    Log.w(
-      "NEUPass",
-      "loginMobileApiTicket deprecated, should be replaced with loginPersonalApiTicket"
-    )
-    return loginNEUAppTicket(portalTicket, "https://portal.neu.edu.cn/mobile/api/sso/login")
-  }
-
   suspend fun loginPersonalApiTicket(portalTicket: String): String {
     return loginNEUAppTicket(
       portalTicket,
@@ -180,12 +131,6 @@ class NEUPass {
 
   suspend fun newECodeSession(): NEUAppSession {
     return newNEUAppSession("https://ecode.neu.edu.cn/ecode/api/sso/login")
-  }
-
-  @Deprecated("Deprecated in 3.x api")
-  suspend fun newMobileApiSession(): NEUAppSession {
-    Log.w("NEUPass", "newMobileApiSession deprecated")
-    return newNEUAppSession("https://portal.neu.edu.cn/mobile/api/sso/login")
   }
 
   private suspend fun loginNEUApp(
@@ -242,17 +187,6 @@ class NEUPass {
       appTicket = eCodeTicket,
       loginUrl = "https://ecode.neu.edu.cn/ecode/api/sso/login",
       redirectUrl = "https://ecode.neu.edu.cn/ecode/#/"
-    )
-  }
-
-  @Deprecated("Deprecated in 3.x api")
-  suspend fun loginMobileApi(session: NEUAppSession, mobileApiTicket: String) {
-    Log.w("NEUPass", "loginMobileApi deprecated, should be replaced with loginPersonalApi")
-    return loginNEUApp(
-      session = session,
-      appTicket = mobileApiTicket,
-      loginUrl = "https://portal.neu.edu.cn/mobile/api/sso/login",
-      redirectUrl = "https://portal.neu.edu.cn/mobile/api"
     )
   }
 
@@ -377,32 +311,6 @@ class NEUPass {
     }
   }
 
-  private suspend inline fun <reified T : PagedResponse<U>, U : PagedResponseItem> pagedAppRequest(
-    session: NEUAppSession,
-    url: String,
-    pageOffset: Int = 0,
-    pageLimit: Int = 10,
-    sort: String = "-status,-createTime",
-    otherParams: String = ""
-  ): T {
-    var url1 = "$url?page[offset]=$pageOffset&page[limit]=$pageLimit&sort=$sort"
-    if (otherParams.isNotEmpty()) {
-      url1 += "&$otherParams"
-    }
-    val response: T = basicAppRequest(
-      session,
-      url1
-    )
-    // 写入 idx 用于分页库计算
-    var idx = pageOffset
-    response.data.forEach {
-      it.idx = idx
-      idx++
-    }
-    Log.d("NEUPass", response.toString())
-    return response
-  }
-
   suspend fun getQRCode(session: NEUAppSession): ListedResponse<ECodeResponse> {
     return basicAppRequest(session, "https://ecode.neu.edu.cn/ecode/api/qr-code")
   }
@@ -434,75 +342,6 @@ class NEUPass {
     return basicPersonalApiRequest(
       session,
       "https://personal.neu.edu.cn/portal/personal/frontend/data/detail?id=$id"
-    )
-  }
-
-  @Deprecated("Use getUserInfo instead")
-  suspend fun getMobileApiUser(session: NEUAppSession): MobileApiUser {
-    return basicAppRequest(session, "https://portal.neu.edu.cn/mobile/api/user")
-  }
-
-  @Deprecated("Use getUserInfo instead")
-  suspend fun getMobileApiUserInfo(session: NEUAppSession): MobileApiUserInfo {
-    return basicAppRequest(session, "https://portal.neu.edu.cn/mobile/api/user/info")
-  }
-
-  @Deprecated("Use getPersonalDataIds & getPersonalDataItem instead")
-  suspend fun getMobileApiCampusCard(session: NEUAppSession): MobileApiCampusCard {
-    return basicAppRequest(session, "https://portal.neu.edu.cn/mobile/api/personal/campusInfo/card")
-  }
-
-  @Deprecated("Use getPersonalDataIds & getPersonalDataItem instead")
-  suspend fun getMobileApiCampusNetwork(session: NEUAppSession): MobileApiCampusNetwork {
-    return basicAppRequest(
-      session,
-      "https://portal.neu.edu.cn/mobile/api/personal/campusInfo/network"
-    )
-  }
-
-  suspend fun getMessageSources(session: NEUAppSession): MessageSourcesResponse {
-    return basicAppRequest(
-      session,
-      "https://portal.neu.edu.cn/mobile/api/message/sources/sourceList"
-    )
-  }
-
-  suspend fun getTasks(session: NEUAppSession, page: Int, pageLimit: Int): PagedResponse<Task> {
-    return pagedAppRequest(
-      session,
-      "https://portal.neu.edu.cn/mobile/api/workrecord/tasks",
-      pageOffset = page * 10,
-      pageLimit = pageLimit
-    )
-  }
-
-  suspend fun getNotifications(
-    session: NEUAppSession,
-    page: Int,
-    pageLimit: Int
-  ): PagedResponse<Notification> {
-    return pagedAppRequest(
-      session,
-      "https://portal.neu.edu.cn/mobile/api/message/notifications",
-      pageOffset = page * 10,
-      pageLimit = pageLimit
-    )
-  }
-
-  suspend fun getArticles(
-    session: NEUAppSession,
-    contentId: Int,
-    page: Int,
-    pageLimit: Int
-  ): PagedResponse<Article> {
-    //Log.d("NEUPass", "获取文章 $contentId 第 $page 页，每页 $pageLimit 条")
-    return pagedAppRequest(
-      session,
-      "https://portal.neu.edu.cn/mobile/api/neunews/articles",
-      sort = "-wbdate",
-      otherParams = "filter[contentid]=$contentId",
-      pageOffset = page * 10,
-      pageLimit = pageLimit
     )
   }
 }
